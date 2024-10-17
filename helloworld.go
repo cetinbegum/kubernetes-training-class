9c37c9e8d17ead8033def7b305cf6887111274ff
cetinbegum <98752460+cetinbegum@users.noreply.github.com>
2024-10-17 12:53:39 +0000

    go düzenlendi

--- helloworld.go
+++ helloworld.go
@@ -1,3 +1,10 @@
 package main
 
-import
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World! :)")
+	fmt.Println("Hello, World! 1")
+	fmt.Println("Hello, World! 2")
+	fmt.Println("Hello, World! 3")
+}
